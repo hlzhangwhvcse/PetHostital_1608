ff11f1b06e1513a72d82fc18fb96c5c03a673ad0
hlzhangwhvcse <34358789+hlzhangwhvcse@users.noreply.github.com>
2018-04-10 12:01:06 +0800

    add a column 'description' in t_speciality

---
+++ Dump20180410.sql
@@ -0,0 +1,128 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_ph
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `t_speciality`
+--
+
+DROP TABLE IF EXISTS `t_speciality`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_speciality` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_speciality`
+--
+
+LOCK TABLES `t_speciality` WRITE;
+/*!40000 ALTER TABLE `t_speciality` DISABLE KEYS */;
+INSERT INTO `t_speciality` VALUES (1,'Pet Cat',NULL),(2,'Pet Dog',NULL),(3,'Pet Bird',NULL),(4,'Pet Fish','cure pet fish'),(5,'Pet Bear','cure pet Bear'),(7,'Pet Duck','cure pet duck');
+/*!40000 ALTER TABLE `t_speciality` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_user`
+--
+
+DROP TABLE IF EXISTS `t_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `pwd` varchar(45) NOT NULL,
+  `tel` varchar(45) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_user`
+--
+
+LOCK TABLES `t_user` WRITE;
+/*!40000 ALTER TABLE `t_user` DISABLE KEYS */;
+INSERT INTO `t_user` VALUES (1,'admin','admin','admin','888','aaa');
+/*!40000 ALTER TABLE `t_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_vet`
+--
+
+DROP TABLE IF EXISTS `t_vet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_vet` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_vet`
+--
+
+LOCK TABLES `t_vet` WRITE;
+/*!40000 ALTER TABLE `t_vet` DISABLE KEYS */;
+INSERT INTO `t_vet` VALUES (4,'zhangsan'),(5,'lisi'),(6,'wangwu'),(7,'zhaoliu'),(8,'xiaozhang'),(9,'å°å¼ '),(10,'xiaoli');
+/*!40000 ALTER TABLE `t_vet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_vet_speciality`
+--
+
+DROP TABLE IF EXISTS `t_vet_speciality`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_vet_speciality` (
+  `vetId` int(11) NOT NULL,
+  `specId` varchar(45) NOT NULL,
+  PRIMARY KEY (`vetId`,`specId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_vet_speciality`
+--
+
+LOCK TABLES `t_vet_speciality` WRITE;
+/*!40000 ALTER TABLE `t_vet_speciality` DISABLE KEYS */;
+INSERT INTO `t_vet_speciality` VALUES (1,'1'),(1,'2'),(2,'2'),(3,'3'),(4,'1'),(5,'1'),(5,'2'),(5,'3'),(6,'1'),(6,'2'),(7,'1'),(7,'2'),(7,'3'),(7,'4'),(7,'5'),(7,'7'),(8,'1'),(8,'3'),(8,'5'),(8,'7'),(9,'1'),(9,'3'),(9,'5'),(10,'2'),(10,'4'),(10,'5'),(10,'7');
+/*!40000 ALTER TABLE `t_vet_speciality` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-10 12:00:16
