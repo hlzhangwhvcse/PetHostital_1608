aaaf83fc67c31ca42bc63a594aee52841f357cda
hlzhangwhvcse <25538330@qq.com>
2018-05-22 11:55:41 +0800

    1)增加了病历查询页面和病例查询结果页面 2）增加了病历管理的业务逻辑VisitServlet,实现了根据宠物名字查询所有宠物信息

---
+++ src/ph/servlet/VisitServlet.java
@@ -0,0 +1,61 @@
+package ph.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import java.util.List;
+
+
+import ph.dao.PetDAO;
+//import ph.dao.VetDAO;
+import ph.po.Pet;
+//import src.ph.dao.VisitDAO;
+//import ph.po.Vet;
+//import src.ph.po.Visit;
+
+//@WebServlet(name = "VisitServlet")
+@WebServlet("/VisitServlet")
+public class VisitServlet extends HttpServlet
+{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String mode = request.getParameter("mode");
+        if("search".equals(mode))
+        {
+            searchPet(request, response);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+    }
+
+    private void searchPet(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException
+    {
+        String petName = request.getParameter("petName");
+        try
+        {
+            List<Pet> pets = new PetDAO().search(petName, "");
+            if(0==pets.size())
+            {
+                request.setAttribute("msg", "没有查到宠物信息");
+                request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+            }
+            else
+            {
+                request.setAttribute("pets", pets);
+                request.getRequestDispatcher("/visitSearchResult.jsp").forward(request, response);
+            }
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+        }
+    }
+}
---
+++ web/visitSearch.jsp
@@ -0,0 +1,40 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hlzhang
+  Date: 2018/5/22
+  Time: 11:43
+  To change this template use File | Settings | File Templates.
+--%>
+<html>
+<head>
+    <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+    <title>病例查询页面</title>
+    <link href="styles.css" rel="stylesheet"/>
+</head>
+<body>
+<div>
+    <div id="header"><%@ include  file="inc/header.inc"%>
+    </div>
+    <div id="main">
+        <form action="VisitServlet" method="post">
+            <input type="hidden" name="mode" value="search">
+            <!-- type="hidden"  隐藏表单控件  用来传递值  这个控件在前台页面不显示 -->
+            <table>
+                <tr>
+                    <td>宠物名字</td>
+                    <td><input  name="petName" title="按宠物名字查询病例信息"/></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input  type="submit" value="查询" /> <input type="reset" value="清空"/></td>
+                </tr>
+            </table>
+        </form>
+        <h4 align="center" style="color:red" ><%=request.getAttribute("msg")==null?"":request.getAttribute("msg") %></h4>
+    </div>
+    <div id="footer">
+        <%@ include  file="inc/footer.inc"%>
+    </div>
+</div>
+</body>
+</html>
